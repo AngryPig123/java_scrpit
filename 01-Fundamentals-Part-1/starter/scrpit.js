18edd69c03fce7f38241dd5782b8e68e82dee0d9
Angrypig123 <shguddnr3@naver.com>
2023-01-21 15:27:31 +0900

    first commit

---
+++ 01-Fundamentals-Part-1/starter/scrpit.js
@@ -0,0 +1,132 @@
+// alert(123);
+// console.log('1' - 1);
+
+//  5 falsy values : 0, ``, undefined, null, Nan => Boolean false value
+// console.log(Boolean(0));
+
+/*
+const money = 0;
+if (money) {
+    console.log(`true`);
+} else {
+    console.log(`false`);
+}
+
+let height = 2;
+if (height || height == 0) {
+    console.log("YAY! Height is defined");
+} else {
+    console.log("YAY! Height is undefined");
+}
+ */
+
+/* const age = 19;
+if (age === 19) {
+    console.log('You just bacame an adult : D');
+}
+
+
+const favoriteNumber = Number(
+    prompt(`What's your favourite number?`)
+);
+
+console.log(favoriteNumber);
+console.log(typeof favoriteNumber);
+
+if (favoriteNumber === 23) {
+    console.log(`Cool! 23 is an amazing number`);
+} else if (favoriteNumber === 7) {
+    console.log(`Cool! 7 is an lucky number`);
+} else {
+    console.log(`Number is not 23 or 7`);
+}
+ */
+
+/* let teamA_1 = Number(
+    prompt(`insert team A-1 score`)
+);
+if (teamA_1 > 100) {
+    alert(`Score <= 100`);
+    teamA_1 = Number(
+        prompt(`insert team A-1 score`)
+    );
+}
+console.log(teamA_1); */
+
+
+// const teamA_2 = Number(
+//     prompt(`insert team A-2 score`)
+// );
+
+// const teamA_3 = Number(
+//     prompt(`insert team A-3 score`)
+// );
+
+// // Average  :   평균
+// const teamA_average = (teamA_1 + teamA_2 + teamA_3) / 3;
+// console.log(teamA_average);
+
+// const teamB_1 = Number(
+//     prompt(`insert team B-1 score`)
+// );
+
+// const teamB_2 = Number(
+//     prompt(`insert team B-2 score`)
+// );
+
+// const teamB_3 = Number(
+//     prompt(`insert team B-3 score`)
+// );
+
+// // Average  :   평균
+// const teamB_average = (teamB_1 + teamB_2 + teamB_3) / 3;
+// console.log(teamB_average);
+
+// if ()
+
+
+const day = `thursday`;
+
+switch (day) {
+
+    case `monday`:
+        console.log(`Plan course sturcture`);
+        break;
+
+    case `tuesday`:
+        console.log(`Prepare theory videos`);
+        break;
+
+    case `whdnesday`:
+    case `thursday`:
+        console.log(`Write code examples`);
+        break;
+
+    default:
+        console.log(`Not a valid day`);
+
+}
+
+/*
+let start = 10;
+while (start != 0) {
+    console.log(start);
+    start--;
+} */
+
+/*
+const test2 = function () {
+    if (5 > 2) {
+        const test = 3;
+        console.log(test);
+    }
+}
+console.log(`test ${test2()}`);
+*/
+
+
+// console.log(`I like to drink ${3 > 5 ? true : false}`);
+
+
+
+
