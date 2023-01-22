cabae10636877df103d375c74c5edaecc4a0d5b1
Angrypig123 <shguddnr3@naver.com>
2023-01-22 18:54:39 +0900

    basic game part1

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+/* 
+//  class .
+const message = document.querySelector('.message');
+
+//  
+console.log(message.textContent);
+
+message.textContent = `🎂Correct Number!`;
+console.log(message.textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+const inputValue = document.querySelector('.guess').value;
+
+ */
+const message = document.querySelector('.message');
+document.querySelector('.check')
+    .addEventListener(
+        'click', function () {
+            const guessValue =
+                Number(document.querySelector('.guess').value);
+            if (!guessValue) {
+                message.textContent = '🥵 No number';
+            } else {
+                message.textContent = 'Start guessing...';
+            }
+        }
+    );
+
+
+
