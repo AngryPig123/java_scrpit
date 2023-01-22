27a94004689270c93f912f827373d58df6f81823
Angrypig123 <shguddnr3@naver.com>
2023-01-22 18:07:52 +0900

    session3 success

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,28 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const calcTempAmplitude = function (temps) {
+
+    let max = temps[0];
+    let min = temps[0];
+
+    for (let i = 1; i < temps.length; i++) {
+        const curTemp = temps[i];
+        if (typeof curTemp !== `number`) continue;
+        if (curTemp > max) max = curTemp;
+        if (curTemp < min) min = curTemp;
+    }
+
+    const minMax = {
+        max: max,
+        min: min,
+        amplitude: max - min
+    }
+
+    return minMax;
+
+}
+
+console.log(calcTempAmplitude([3, 15, 6, -7, `test`]));
+
+
