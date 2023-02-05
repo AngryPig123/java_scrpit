70f17fae07011f1467e4886b82b27f0c0b99a83d
Angrypig123 <shguddnr3@naver.com>
2023-02-06 07:17:52 +0900

    .

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -28,6 +28,71 @@
  */
 
 
-    
-//  bind method
+/* 
+    //  bind method
+    const oneWord = function (str) {
+        return str.replace(/ /g, '').toLowerCase();
+    }
 
+    const upperFirstWord = function (str) {
+        const [first, ...others] = str.split(' ');
+        return [first.toUpperCase(), ...others].join(' ');
+    }
+
+    // higher order function
+    const transformer = function (str, fn) {
+        return fn(str);
+    }
+
+    const hey = function (greet) {
+        return function (name) {
+            console.log(`${greet}, ${name}`);
+        }
+    }
+    hey('greet message')('hey name');
+ */
+const lufthansa = {
+    airline: 'Lufthansa',
+    iataCode: 'LH',
+    bookings: [],
+    book(fligthNum, name) {
+        console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${fligthNum}`);
+        this.bookings.push(
+            {
+                flight: `${this.iataCode}${fligthNum}`,
+                name: name
+            }
+        )
+    },
+};
+lufthansa.book(3, `Jonas`);
+console.log(lufthansa.bookings);
+
+const eurowings = {
+    airline: 'Eurowings',
+    iataCode: 'EW',
+    bookings: [],
+    book(fligthNum, name) {
+        console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${fligthNum}`);
+        this.bookings.push(
+            {
+                flight: `${this.iataCode}${fligthNum}`,
+                name: name
+            }
+        )
+    },
+};
+
+// Does NOT work
+const book = lufthansa.book;
+
+book.call(lufthansa, 23, 23);
+console.log(lufthansa);
+
+const swiss = {
+    name: 'Swiss air line',
+    iataCode: 'LX',
+    bookings: []
+}
+book.call(swiss, 23, 23);
+console.log(swiss);
